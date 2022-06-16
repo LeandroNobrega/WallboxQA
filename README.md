4d0600570293a62ebba69105738d33654a985659
Leandro Nobrega <leandro.mnobrega@gmail.com>
2022-06-15 23:09:59 -0300

    SOLUTION1: Uploaded solution for item1
    - Added folder structure
    - Added item1.py and test_item1.py
    - Updated README

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
-# Wallbox QA Coding Test Solution
+# Wallbox QA Coding Test Solution: Unit Testing
 
 This repo contains a solution for the Technical Test provided by Wallbox
 
-## Part I: Unit Testing
+## PROBLEMS
 
 Write code to solve the following problems:
 
@@ -10,8 +10,11 @@ Write code to solve the following problems:
 
 2. A function that given a path of the file system finds the first file that meets the
 following requirements
+
 a. The file owner is admin
+
 b. The file is executable
+
 c. The file has a size lower than 14*2^20
 
 3. A function that given a sequence of coin flips (0 is tails, 1 is heads) finds the
@@ -20,4 +23,27 @@ example, given the sequence 0,1,1,0 how many changes are needed so that the
 result is 0,1,0,1
 
 
-PS.: Incluse Pytest unit testing with at least 2 Tests
+PS.: Include Pytest unit testing with at least 2 Test Cases
+
+
+## SOLUTION
+
+The solution for the three problems presented is detaild below:
+
+### Item 1
+
+File: src/item1.py
+
+Usage:
+find_repeated_numbers(): Parametrized Function that receives two lists and returns an integer with the first repeated number.
+"First" is interpreted here as the lowest index among any of the two repeated elements.
+
+### Item 1: PYTEST
+
+File: test/test_item1.py
+
+Usage: pytest -v test/test_item.py (7 Tests)
+
+Each Test Case is documented as docstring on the test file.
+
+ 
---
+++ tests/test_item1.py
@@ -0,0 +1,48 @@
+import random
+from solution1 import find_repeated_numbers, EntryListException
+
+
+def test_basic():
+    # Basic test, same size lists
+    entry1 = [1, 2, 3, 4]
+    entry2 = [0, 1, 2, 3]
+    assert find_repeated_numbers(entry1, entry2) == 1, "Failed to find the first repeated number"
+
+def test_diff_sizes():
+    # Basic test, different size lists
+    entry1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    entry2 = [9, 0, 8, 1]
+    assert find_repeated_numbers(entry1, entry2) == 1, "Failed to find the first repeated number"
+
+def test_size_one():
+    # Second list has size 1
+    entry1 = [1, 2, 3, 4, 5, 6, 7, 8]
+    entry2 = [5]
+    assert find_repeated_numbers(entry1, entry2) == 5, "Failed to find the first repeated number"
+
+def test_empty_entry():
+    # First list is empty
+    entry1 = []
+    entry2 = [0, 9, 8, 7, 6]
+    assert not find_repeated_numbers(entry1, entry2), "Function should have returned None"
+
+def test_different_entries():
+    entry1 = [1, 2, 3, 4, 5]
+    entry2 = [6, 7, 8, 9, 0]
+    assert not find_repeated_numbers(entry1, entry2), "Function should have returned None"
+
+def test_wrong_entry():
+    # Second entry has a string
+    entry1 = [1, 2, 3, 4, 5]
+    entry2 = [6, 7, "A", 9, 0]
+    try:
+        assert find_repeated_numbers(entry1, entry2), "Function should have raised EntryListException"
+    except EntryListException:
+        pass
+
+def test_random_entry():
+    # Test to analyze execution of larger numbers
+    size = random.randint(0, 100)
+    random_entry1 = [random.randint(0, size) for _ in range(size)]
+    random_entry2 = [random.randint(0, size) for _ in range(size)]
+    find_repeated_numbers(random_entry1, random_entry2)
